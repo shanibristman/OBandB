48c45696d310539cb1b2dd30842b8d2e2f086e47
shanibristman <shanibristman@gmail.com>
2022-05-29 09:32:37 +0300

    start

---
+++ models/note.js
@@ -0,0 +1,68 @@
+
+const DB = require('../utils/db');
+
+class Note {
+    title;
+    description;
+    createdAt;
+    isActive;
+
+    constructor(title = "", description = "") {
+        this.title = title;
+        this.description = description;
+        this.createdAt = Date.now();
+        this.isActive = true;
+    }
+
+    async GetAllActiveNotes() {
+        try {
+            return await new DB().FindAll('notes', { isActive: true });
+        } catch (error) {
+            return error;
+        }
+    }
+
+    async GetAllNotes() {
+        try {
+            return await new DB().FindAll('notes');
+        } catch (error) {
+            return error;
+        }
+    }
+
+    async GetNoteByID(id) {
+        try {
+            return await new DB().FindByID('notes', id);
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    }
+
+    async InsertNewNote() {
+        try {
+            return await new DB().Insert('notes', this); 
+        } catch (error) {
+            return error;
+        } 
+    }
+
+    async UpdateNoteById(id) {
+        try {
+            return await new DB().UpdateDocById('notes', id, this);
+        } catch (error) {
+            console.log(error);
+            return error;
+        } 
+    }
+
+    async DeleteNote(id) {
+        try {
+            return await new DB().DeactivateDocById('notes',id);
+        } catch (error) {
+            return error;
+        }
+    }
+}
+
+module.exports = Note;
---
+++ server.js
@@ -0,0 +1,24 @@
+//Libraries
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+//Server initialize
+const PORT = process.env.PORT || 5008;
+const server = express();
+server.use(cors()); //allow us to access the server from each endpoint
+server.use(express.json()); //add json support for POST, GET, PUT, DELETE 
+server.use(express.static(path.join(__dirname, 'client/build/')));
+
+//routes
+server.use('/api/notes', require('./controllers/noteController'));
+
+//Global Get Request
+server.get('*/*', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/', 'index.html'));
+});
+
+//Run the server
+server.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
