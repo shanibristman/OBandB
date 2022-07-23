4542c270fdcc8f50fc70b133df737a51ae433439
Alon Shalev <alonshalev43@gmail.com>
2022-07-23 14:17:20 +0300

    uplaod image function in server

--- server.js
+++ server.js
@@ -3,15 +3,27 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-
+const multer=require('multer');
 //Server initialize
 const PORT = process.env.PORT || 5008;
 const server = express();
+
+const fileStorageEngine=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'./images')
+    },
+    filename:(req,file,cb)=>{
+        cb(null,Date.now()+"--"+file.originalname)
+    },
+})
+const upload =multer({storage:fileStorageEngine});
+
 server.use(cors()); //allow us to access the server from each endpoint
 server.use(express.json()); //add json support for POST, GET, PUT, DELETE 
 server.use(express.static(path.join(__dirname, 'client/build/')));
 
 //routes
+
 server.use('/api/notes', require('./controllers/noteController'));
 server.use('/api/users', require('./controllers/UserController'));
 server.use('/api/attraction', require('./controllers/AttractionController'));
@@ -19,8 +31,16 @@ server.use('/api/business', require('./controllers/BusinessController'));
 server.use('/api/category', require('./controllers/CategoryController'));
 
 
+// upload request first try wish luck god
+server.post('/single',upload.single('image'),(req,res)=>{
+    console.log(req.file);
+    res.send("single File upload success");
+});
 
-
+server.post('/multiple',upload.array('images',3),(req,res)=>{
+console.log(req.file);
+res.send("multiple files upload success");
+});
 //Global Get Request
 server.get('*/*', async (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build/', 'index.html'));
